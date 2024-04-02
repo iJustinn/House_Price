8f2a769bd5c3e07bf4c02c91d3be5965ccebe3e7
iJustinn <ijustinzhong@gmail.com>
2024-04-01 22:14:35 -0400

    data cleaning script updated, ocean_proximity_frequency data added

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -28,10 +28,35 @@ cali_house_price_clean <- cali_house_price %>%
 
 
 
+cali_house_price_clean %>% 
+  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>% 
+  walk2(names(.), ~cat("The mean value of", .y, ":", .x, "\n")) # Calculating and printing mean of each numeric column
+
+
+
+columns_needed <- c("ocean_proximity") # Specify the needed columns
+
+ocean_proximity_frequency <- data.frame(Variable = character(), Frequency = integer()) # Initialize an empty data frame for storing frequencies
+
+for (j in columns_needed) {
+  temp <- as.data.frame(table(cali_house_price_clean[[j]]))  # Calculate frequency using table and convert to data frame
+  
+  colnames(temp) <- c("Variable", "Frequency")  # Rename the columns accordingly
+  
+  ocean_proximity_frequency <- bind_rows(ocean_proximity_frequency, temp)  # Combine with the main dataset
+}
+
+print(ocean_proximity_frequency) # Show the resulting dataset
+
+
+
 #### save data ####
 write_csv(
   x = cali_house_price_clean,
   file = "outputs/data/cali_house_price_clean.csv"
 )
 
-
+write_csv(
+  x = ocean_proximity_frequency,
+  file = "outputs/data/ocean_proximity_frequency.csv"
+)
